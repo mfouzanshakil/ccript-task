145a98e8c15a4c320e90a5a43c80f3c80edc705a
mfouzanshakil <mfznshakil2222@gmail.com>
2024-01-20 22:34:48 +0500

    login form finalized

--- src/auth/login/LoginForm.jsx
+++ src/auth/login/LoginForm.jsx
@@ -1,39 +1,71 @@
 import React from "react";
+import { useRouter } from "next/router";
 import { Box, TextField, Button, CardMedia } from "@mui/material";
-import logo from "../../../assets/ccript-logo.jpg";
+import logo from "../../../public/ccript-logo.jpg";
+import { Formik, Form, Field } from "formik";
 
 const LoginForm = () => {
-    
+  const router = useRouter();
+  const handleFormSubmit = (values) => {
+    console.log("values", values);
+    router.push("/main");
+  };
+
   return (
     <Box
       sx={{
         display: "flex",
         flexDirection: "column",
         alignItems: "center",
-        mt: "20%",
+        mt: "10%",
       }}
     >
-      <Box component="img" alt="CCript Logo" src={logo} />
-      <CardMedia image={logo} />
-      <Box component="form" sx={{ display: "flex", flexDirection: "column" }}>
-        <TextField
-          label="Email"
-          variant="filled"
-          sx={{ mb: 2 }}
-          type={"email"}
-          color="success"
-        />
-        <TextField
-          label="Password"
-          variant="filled"
-          sx={{ mb: 5 }}
-          type={"password"}
-          color="success"
-        />
-        <Button variant="contained" color="success">
-          Sign In
-        </Button>
-      </Box>
+      <Formik
+        initialValues={{ email: "", password: "" }}
+        onSubmit={handleFormSubmit}
+      >
+        <Form style={{ display: "flex", flexDirection: "column" }}>
+          <Box sx={{ maxHeight: "50%", maxWidth: "50%" }}>
+            <img src={logo.src} />
+          </Box>
+          <Field
+            type="text"
+            id="email"
+            name="email"
+            placeholder="Enter your username"
+            render={({ field }) => (
+              <TextField
+                {...field}
+                label="Email"
+                variant="filled"
+                sx={{ mb: 2 }}
+                type={"email"}
+                color="success"
+              />
+            )}
+          />
+
+          <Field
+            type="text"
+            id="password"
+            name="password"
+            placeholder="Enter your password"
+            render={({ field }) => (
+              <TextField
+                {...field}
+                label="Password"
+                variant="filled"
+                sx={{ mb: 5 }}
+                type={"text"}
+                color="success"
+              />
+            )}
+          />
+          <Button type="submit" variant="contained" color="success">
+            Sign In
+          </Button>
+        </Form>
+      </Formik>
     </Box>
   );
 };
